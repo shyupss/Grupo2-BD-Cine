e0133587b69ddff16d7a59eaf33c28f9dcbb58d7
cris-tobal <jcristobal2002@gmail.com>
2025-06-25 22:19:30 -0400

    eliminando carpetas q ya no nos sirven...

--- own/tk_frame.py
+++
@@ -1,6 +0,0 @@
-from . import tk
-
-class Frame(tk.Frame):
-
-    def __init__(self, master:tk.Tk|tk.Toplevel|tk.Frame|tk.Canvas, **kwargs):
-        super().__init__(master, **kwargs)
--- own/tk_textframe.py
+++
@@ -1,8 +0,0 @@
-from . import tk
-
-from tkinter.scrolledtext import ScrolledText
-
-class TextFrame(ScrolledText):
-
-    def __init__(self, master:tk.Tk|tk.Toplevel|tk.Frame|tk.Canvas, **kwargs):
-        super().__init__(master, **kwargs)
